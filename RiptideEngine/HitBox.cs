b2e2e8f036739b314bf98e1ebb51e4dec81ed74f
auxy6858 <cambee093@gmail.com>
2024-12-23 00:00:36 +0000

    Initial commit

---
+++ RiptideEngine/HitBox.cs
@@ -0,0 +1,39 @@
+﻿namespace RiptideEngine;
+
+using Microsoft.Xna.Framework;
+
+public struct HitBox : IEquatable<HitBox>, IComparable<HitBox>
+{
+    public Rectangle HitBoxRectangle { get; set; }
+
+    public bool Equals(HitBox other)
+    {
+        return HitBoxRectangle == other.HitBoxRectangle;
+    }
+
+    public int CompareTo(HitBox other)
+    {
+        return (other.HitBoxRectangle.Height * other.HitBoxRectangle.Width).CompareTo(HitBoxRectangle.Height * HitBoxRectangle.Width);
+    }
+}
+
+public struct CircleHitBox : IEquatable<CircleHitBox>, IComparable<CircleHitBox>
+{
+    private Vector2 Origin { get; set; }
+    private float Radius { get; set; }
+
+    public bool CollidesWith(Vector2 point)
+    {
+        return ( Math.Pow(point.X - Origin.X, 2) + Math.Pow(point.Y - Origin.Y, 2) <= Math.Pow(Radius, 2) );
+    }
+
+    public bool Equals(CircleHitBox other)
+    {
+        return (other.Origin == Origin) && (Math.Abs(other.Radius - Radius) < 1e-3f);
+    }
+
+    public int CompareTo(CircleHitBox other)
+    {
+        return Radius.CompareTo(other.Radius);
+    }
+}
