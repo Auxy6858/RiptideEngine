fddf3f33facacaea350c56b6d1aec1460a83cc32
Cameron <cambee093@gmail.com>
2025-01-05 11:06:33 +0000

    Added GUI project files

---
+++ RiptideGUI/Program.cs
@@ -0,0 +1,30 @@
+﻿using Avalonia;
+using System;
+using RiptideEngine;
+
+namespace RiptideGUI;
+
+class Program
+{
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
+    
+    
+    
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace();
+    
+    public static void PrintGreeting()
+    {
+        Console.WriteLine($"{EngineInfo.Name} version {EngineInfo.Version} \n { EngineInfo.Motd[ Random.Shared.Next(0,2) ] }");
+    }
+}
