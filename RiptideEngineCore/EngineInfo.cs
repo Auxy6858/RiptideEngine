2442864ef7aa2f5088727e4586e3c8e8eabc0f8a
Cameron <cambee093@gmail.com>
2025-01-05 11:05:06 +0000

    Started work on GUI

---
+++ RiptideEngineCore/EngineInfo.cs
@@ -0,0 +1,16 @@
+namespace RiptideEngine;
+
+public static class EngineInfo
+{
+    public static string Version = "1.0.0";
+    public static string VersionName = "SnowFlake";
+    public static string Name = "RiptideEngine";
+    
+    public static Dictionary<int, string> Motd = new Dictionary<int, string>()
+    {
+        {0, "Falling from the sky."},
+        {1, "Made with <3 by Auxy6858"},
+        {2, "Uses FNA https://fna-xna.github.io"},
+    };
+
+}
--- RiptideEngineCore/obj/Debug/net9.0/RiptideEngine.AssemblyInfo.cs
+++ RiptideEngineCore/obj/Debug/net9.0/RiptideEngine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RiptideEngine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2e2e8f036739b314bf98e1ebb51e4dec81ed74f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa8d8b62ea843072930a27bbaf2abc17a0ace0bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("RiptideEngine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RiptideEngine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
